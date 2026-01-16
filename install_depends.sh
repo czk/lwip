1f884efe8800f88e46b8e50124d7c805c895d7dc
czk <czkfsx@gmail.com>
2026-01-16 07:47:03 +0000

    优化脚本路径

--- install_depends.sh
+++ install_depends.sh
@@ -4,8 +4,9 @@
 #
 # 准备环境安装依赖项
 set -e
+SCRIPT_DIR="$( dirname "$(realpath "${BASH_SOURCE[0]}")" )"
 
-cp -f /workspaces/lwip/contrib/examples/example_app/lwipcfg.h.example /workspaces/lwip/contrib/examples/example_app/lwipcfg.h
+cp -f ${SCRIPT_DIR}/contrib/examples/example_app/lwipcfg.h.example ${SCRIPT_DIR}/contrib/examples/example_app/lwipcfg.h
 sudo npm install -g @anthropic-ai/claude-code
 sudo apt update
 sudo apt install check -y
